683bebbb9f09f951a96ec3346e5b8d1dc1204471
Aviad Blumenfeld <aviadblu@gmail.com>
2021-01-05 11:29:14 +0200

    Add gaphql server

---
+++ server/resolvers.js
@@ -0,0 +1,16 @@
+const db = require('./db')
+
+const Query = {
+    job: (root, {id}) => db.jobs.get(id),
+    jobs: async () => {
+        return db.jobs.list()
+    }
+}
+
+const Job = {
+    company: (job) => db.companies.get(job.companyId)
+}
+module.exports = {
+    Query,
+    Job
+}
